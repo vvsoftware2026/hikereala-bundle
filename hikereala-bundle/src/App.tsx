17cbadea1df85e06531433c1594178dc17c92930
Jurj Vlad <jurjvlad96@gmail.com>
2025-12-28 20:15:06 +0200

    misc

--- hikereala-bundle/src/App.tsx
+++ hikereala-bundle/src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     Welcome to Hikereala bundle, the future in modern hikereala software. Developed with love and barbataureala.
+     Welcome to Hikereala bundle, the future in modern hikereala software. Developed with lots of love and barbataureala.
     </>
   )
 }
