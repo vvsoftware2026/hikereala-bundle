cceecdcaadc1a16ad08729b24a3b085d137256e8
Jurj Vlad <jurjvlad96@gmail.com>
2025-12-28 20:12:04 +0200

    Initial commit

---
+++ hikereala-bundle/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     Welcome to Hikereala bundle, the future in modern hikereala software. Developed with love and barbataureala.
+    </>
+  )
+}
+
+export default App
